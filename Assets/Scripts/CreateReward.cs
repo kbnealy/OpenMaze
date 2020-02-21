6ad4a3faafaac248498d288a2037d2d13eea7e1e
kbnealy <k.b.nealy@gmail.com>
2020-02-21 14:53:16 -0500

    Updated branch with the MM experiment and pilot changes

---
+++ Assets/Scripts/CreateReward.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateReward : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform Spawnpoint;
+    public GameObject Prefab;
+
+    void OnTriggerEnter()
+    {
+        Instantiate(Prefab, Spawnpoint.position, Spawnpoint.rotation);
+    }
+
+}
